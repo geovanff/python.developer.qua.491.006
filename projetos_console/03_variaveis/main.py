179fc5ab2132ac19a7cc28f25c93775be15fb29b
geovanff <geovan.ff@gmail.com>
2025-06-11 19:52:16 -0300

    Primeiro commit. Aula do dia 11/06/2025.

---
+++ projetos_console/03_variaveis/main.py
@@ -0,0 +1,11 @@
+# Declaração de variáveis
+texto = "Este é um texto."
+numero_interio = 42
+numero_decimal = 3.14
+programador = True
+
+# saida de dados
+print(texto)
+print(numero_interio)
+print(numero_decimal)
+print(programador)
---
+++ projetos_console/09_estrutura_de_decisao_parte_04/main.py
@@ -0,0 +1,8 @@
+# Declaração de variáveis
+nome = input("Informe seu nome: ")
+idade = int(input("Informe sua idade: "))
+
+
+result = "é maior de idade." if idade >= 18 else "é menor de idade."
+
+print(f"{nome} {result}")
